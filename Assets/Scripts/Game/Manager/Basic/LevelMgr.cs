bb74584a97bb4303a0da3023d54eee4761395c4b
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-05-04 22:21:22 +0100

    GroundTag

--- Assets/Scripts/Game/Manager/Basic/LevelMgr.cs
+++ Assets/Scripts/Game/Manager/Basic/LevelMgr.cs
@@ -6,12 +6,26 @@ public class LevelMgr : MonoBehaviour
 {
     public CharacterBasic character;
     public Transform tfMonster;
+    public GameObject pfMonster;
 
     public void Init()
     {
         character.Init();
+
+        GenerateTenMon();
     }
 
+    public void GenerateTenMon()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            float posX = Random.Range(-3f, 3f);
+            float posZ = Random.Range(-3f, 3f);
+            GameObject objMonster = GameObject.Instantiate(pfMonster,new Vector3(posX,1F,posZ),Quaternion.identity,tfMonster);
+            MonsterBasic itemMonster = objMonster.GetComponent<MonsterBasic>();
+            itemMonster.Init();
+        }
+    }
 
     public void TimeFixedGoLevel(float time)
     {
