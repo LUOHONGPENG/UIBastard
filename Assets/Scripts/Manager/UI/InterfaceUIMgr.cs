226668022d570259c3d8ae708f7f9c37afdac743
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-05-04 14:50:03 +0100

    Upload

---
+++ Assets/Scripts/Manager/UI/InterfaceUIMgr.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceUIMgr : MonoBehaviour
+{
+    public Button btnSetting;
+
+    public Button btnCharacter;
+
+    public void Init()
+    {
+
+    }
+
+}
--- Assets/Scripts/Manager/UI/UIMgr.cs
+++ Assets/Scripts/Manager/UI/UIMgr.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class UIMgr : MonoBehaviour
 {
+    public InterfaceUIMgr interfaceUIMgr;
+    public void Init()
+    {
 
+    }
 }
