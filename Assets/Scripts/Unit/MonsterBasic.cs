83fa75774a488b36fd84d0d87d1161dc53545a14
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-05-04 16:31:41 +0100

    MonsterSprite

---
+++ Assets/Scripts/Unit/MonsterBasic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBasic : MonoBehaviour
+{
+    public SpriteRenderer srMonster;
+
+    private float curHP;
+
+    public void Init()
+    {
+
+    }
+
+
+}
